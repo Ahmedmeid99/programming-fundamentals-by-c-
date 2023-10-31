8cf4e4fab9303e995d0b5dc78dd80d9d72435202
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2023-10-31 21:00:31 +0200

    add-Basics-2

---
+++ 01-intro to c++ 1/2-c++Basics2/c++Basics2/c++Basics2.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // solve tasks
+    // [1]
+    cout << "**************************\n";
+    cout << "name: ahmed eid.\n" << "Age: 23.\n" <<"City: Egypt.\n";
+    cout << "**************************\n";
+
+    // [2]
+    cout << "***************\n" << "***************\n" << "***************\n" << "***************\n";
+
+    // [3]
+    cout << "I love programing \n\n" << "and use this languages\n" << " c++ \n javascript \n python\n";
+
+    // [4]
+    cout << "*          *\n";
+    cout << "*          *\n";
+    cout << "************\n";
+    cout << "*          *\n";
+    cout << "*          *" << endl;
+        return 0;
+}
