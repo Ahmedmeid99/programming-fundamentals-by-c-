252736d0dc263f234093cee1d4555afa7ee33502
Ahmedmeid99 <ahmedalbakrycool@gmail.com>
2023-11-05 14:26:21 +0200

    24-30

--- 02-algorithms lever_1/problem_solving_24-40/problem_solving_24-40/problem_solving_24-40.cpp
+++ 02-algorithms lever_1/problem_solving_24-40/problem_solving_24-40/problem_solving_24-40.cpp
@@ -1,20 +1,102 @@
-// problem_solving_24-40.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+
+// [24] check if age is valid or not 18 : 45
+void check_age(int &age)
+{
+    cout << "enter your age\n";
+    cin >> age;
+    if (age <= 18 || age >= 45)
+    {
+        cout << age << " invalid age\n\n";
+    }else{ cout << age << " valid age\n\n"; }
+
+}
+
+// [25] enter valid age only
+void enter_valid_age(int &age)
+{
+    do {
+        cout << "enter your age\n";
+        cin >> age;
+    }while(age < 18 || age > 45);
+
+    cout << age << " is valid age\n\n";
+    
+}
+
+// [26] prin from 1 to n (entered number)
+void print_to(int &n)
+{
+    cout << "enter a number to print from 1 to this number\n";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i << endl;
+    }
+}
+
+// [27] prin from n to 1 (n is entered number)
+void print_to_1(int &n)
+{
+    cout << "enter a number to print from n to 1 \n";
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+}
+
+// [28] print Even number to n
+void print_Even(int &n)
+{
+    cout << "enter number ...\n";
+    cin >> n;
+    cout << "the even numbers is \n";
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
+
+// [29] print ODD number to n
+void print_ODD(int &n)
+{
+    cout << "enter number ...\n";
+    cin >> n;
+    cout << "the odd numbers is \n";
+    for (int i = 0; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << i << endl;
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int age1,age2,n1,n2,num1,num2;
+    // [24] check if age is valid or not 18 : 45
+    check_age(age1);
+
+    // [25] enter valid age only
+    enter_valid_age(age2);
+
+    // [26] prin from 1 to n (n is entered number)
+    print_to(n1);
+
+    // [27] prin from n to 1 (n is entered number)
+     print_to_1(n2);
+    
+    // [28] print Even number to n
+    print_Even(num1);
+
+    // [29] print ODD number to n
+    print_ODD(num2);
+    cout << "hello";
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
